23d633c51778695bab5cb07222785c2fcd355cac
George S. Mulbah <gsmulbah2500@gmail.com>
2025-03-26 10:58:34 +0200

    added the solution for the bestime to buy stock

---
+++ array/best_time_to_by_stock.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut max_profit = 0;
+
+        let min_price = i32::MAX;
+
+        for price in prices {
+            min_price = min_price.min(price);
+
+            current_profit = price - min_price;
+
+            max_profit = max_profit.mex(current_profit);
+        }
+
+        max_profit
+    }
+}
